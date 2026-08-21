efff32453e6885000397c784aff2b0effa979bc8
Wanderson Alves Santos <wandersonalves.s@hotmail.com>
2026-08-21 00:29:44 -0300

    chore(release): bump version to 4.0.0 and setup Cypress E2E

---
+++ cypress/e2e/smoke.cy.ts
@@ -0,0 +1,17 @@
+describe('Smoke Test - Agile Space', () => {
+  it('Deve carregar a página inicial corretamente', () => {
+    // Acessa a raiz configurada no baseUrl
+    cy.visit('/');
+    
+    // Verifica se a página renderizou procurando pelo título ou um elemento chave
+    cy.get('body').should('be.visible');
+    cy.title().should('not.be.empty');
+  });
+
+  it('Deve ter a navegação para o Changelog disponível', () => {
+    cy.visit('/');
+    
+    // Procura por um link que leve para a página de changelog
+    cy.get('a[href*="/changelog"]').should('exist');
+  });
+});
